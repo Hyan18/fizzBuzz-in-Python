8118cffdc676b868edbb8ad25bdab9385fd146f3
Hyan18 <HarrisonYan18@gmail.com>
2020-04-24 15:15:31 +0100

    test: handles numbers without a factor of 3

--- test_fizzBuzz.py
+++ test_fizzBuzz.py
@@ -3,5 +3,11 @@ from fizzBuzz import fizzBuzz
 def test_one_returns_one():
     assert fizzBuzz(1) == 1
 
+def test_two_returns_two():
+    assert fizzBuzz(2) == 2
+
 def test_three_returns_fizz():
     assert fizzBuzz(3) == 'fizz'
+
+def test_four_returns_four():
+    assert fizzBuzz(4) == 4
